9eef753c0cb4e97735f3063c1b4ebc42934cee18
Taurean Boyd <trboyd8@gmail.com>
2019-08-04 11:37:48 -0400

    Adding parsing logic for JSON messages and updating the details activity.

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -3,9 +3,14 @@ package com.udacity.sandwichclub;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
@@ -20,7 +25,8 @@ public class DetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        ImageView ingredientsIv = findViewById(R.id.image_iv);
+        final ImageView ingredientsIv = findViewById(R.id.image_iv);
+        final ImageView errorImageIv = findViewById(R.id.error_image_iv);
 
         Intent intent = getIntent();
         if (intent == null) {
@@ -43,20 +49,60 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
-                .into(ingredientsIv);
+                .into(ingredientsIv, new Callback() {
+                    @Override
+                    public void onSuccess() {
+                        ingredientsIv.setVisibility(View.VISIBLE);
+                    }
+
+                    @Override
+                    public void onError() {
+                        errorImageIv.setVisibility(View.VISIBLE);
+                    }
+                });
 
         setTitle(sandwich.getMainName());
     }
 
     private void closeOnError() {
         finish();
-        Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, getString(R.string.detail_error_message), Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    private void populateUI(Sandwich sandwich) {
+        if (sandwich.getAlsoKnownAs().size() > 0) {
+            LinearLayout alsoKnownAsLL = findViewById(R.id.also_known_ll);
+            alsoKnownAsLL.setVisibility(View.VISIBLE);
 
+            TextView alsoKnownAsTV = findViewById(R.id.also_known_tv);
+            alsoKnownAsTV.setText(TextUtils.join(", ", sandwich.getAlsoKnownAs()));
+        }
+
+        if (!sandwich.getDescription().equals("")) {
+            LinearLayout alsoKnownAsLL = findViewById(R.id.description_ll);
+            alsoKnownAsLL.setVisibility(View.VISIBLE);
+
+            TextView descriptionTV = findViewById(R.id.description_tv);
+            descriptionTV.setText(sandwich.getDescription());
+        }
+
+        if (sandwich.getIngredients().size() > 0) {
+            LinearLayout alsoKnownAsLL = findViewById(R.id.ingredients_ll);
+            alsoKnownAsLL.setVisibility(View.VISIBLE);
+
+            TextView ingredientsTV = findViewById(R.id.ingredients_tv);
+            ingredientsTV.setText(TextUtils.join("\n", sandwich.getIngredients()));
+        }
+
+        if (!sandwich.getPlaceOfOrigin().equals("")) {
+            LinearLayout alsoKnownAsLL = findViewById(R.id.origin_ll);
+            alsoKnownAsLL.setVisibility(View.VISIBLE);
+
+            TextView originTV = findViewById(R.id.origin_tv);
+            originTV.setText(sandwich.getPlaceOfOrigin());
+        }
     }
 }
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -1,10 +1,53 @@
 package com.udacity.sandwichclub.utils;
 
+import android.util.Log;
+
 import com.udacity.sandwichclub.model.Sandwich;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class JsonUtils {
 
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+        try {
+            JSONObject sandwichJSON = new JSONObject(json);
+            Sandwich sandwich = new Sandwich();
+
+            JSONObject nameJSON = sandwichJSON.getJSONObject("name");
+            sandwich.setMainName(nameJSON.getString("mainName"));
+            sandwich.setAlsoKnownAs(convertJSONArrayToList(nameJSON.getJSONArray("alsoKnownAs")));
+
+            sandwich.setPlaceOfOrigin(sandwichJSON.getString("placeOfOrigin"));
+            sandwich.setDescription(sandwichJSON.getString("description"));
+            sandwich.setImage(sandwichJSON.getString("image"));
+            sandwich.setIngredients(convertJSONArrayToList(sandwichJSON.getJSONArray("ingredients")));
+
+            return sandwich;
+        }
+        catch (JSONException ex) {
+            Log.w("JSONUtils", "A JSON string we were trying to parse is invalid.");
+            ex.printStackTrace();
+            return null;
+        }
+    }
+
+    private static List<String> convertJSONArrayToList(JSONArray jsonArray) throws JSONException {
+        if (jsonArray == null || jsonArray.length() == 0) {
+            return new ArrayList<String>();
+        }
+
+        int numItemsInArray = jsonArray.length();
+        String[] array = new String[numItemsInArray];
+        for (int i = 0; i < numItemsInArray; i++) {
+            array[i] = (String) jsonArray.get(i);
+        }
+
+        return Arrays.asList(array);
     }
 }
